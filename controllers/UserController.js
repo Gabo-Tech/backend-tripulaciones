5bcbd1f4e5d993426c636734c9a43bda7b319f5f
yorch82 <jcampo1982@outlook.com>
2022-07-15 17:49:11 +0200

    create, login, logout & getAll user endpoints

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -83,7 +83,7 @@ const UserController = {
     try {
       const { page = 1, limit = 10 } = req.query;
       const users = await User.find()
-        .populate("commentIds")
+        // .populate("commentIds")
         .limit(limit * 1)
         .skip((page - 1) * limit);
       res.send(users);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const { authentication } = require("../middleware/Authentication");
 
 router.post('/', UserController.create);
 router.post('/login',UserController.login);
-router.put('/logout', UserController.logout);
+router.put('/logout', authentication, UserController.logout);
 router.get('/getAll', UserController.getAll);
 
 module.exports = router;
