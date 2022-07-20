171cf20735054d085c8eb89b958e480e90964f5a
Dubesor22 <dubesor22@gmail.com>
2022-07-20 12:52:54 +0200

    comments

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -23,6 +23,76 @@ const CommentController = {
       next(error);
     }
   },
+  async delete(req, res) {
+    try {
+      const comment = await Comment.findByIdAndDelete(req.params._id);
+      res.send({ comment, message: "Comentario borrado" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Error al borrar el comentario" });
+    }
+  },
+  async update(req, res) {
+    try {
+      const comment = await Comment.findByIdAndUpdate(
+        req.params._id,
+        req.body,
+        { new: true }
+      );
+      res.send({ message: "actualizado", comment });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async getAll(req, res) {
+    try {
+      const comments = await Comment.find();
+      res.send(comments);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async like(req, res) {
+    try {
+      const comment = await Comment.findById(req.params._id);
+      if (!comment)
+        return res.status(400).send({
+          message: "You cant't give a like to a comment that doesn't exist",
+        });
+      const exist = await Comment.findById(req.params._id);
+      if (!exist.likes.includes(req.user._id)) {
+        const comment = await Comment.findByIdAndUpdate(
+          req.params._id,
+          { $push: { likes: req.user._id } },
+          { new: true }
+        );
+        res.status(200).send(comment);
+      } else {
+        res.status(400).send({ message: "You can't give more likes" });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "There was an issue in the controller" });
+    }
+  },
+  async removeLike(req, res) {
+    try {
+      const exist = await Comment.findById(req.params._id);
+      if (exist.likes.includes(req.user._id)) {
+        const comment = await Comment.findByIdAndUpdate(
+          req.params._id,
+          { $pull: { likes: req.user._id } },
+          { new: true }
+        );
+        res.status(200).send(comment);
+      } else {
+        res
+          .status(400)
+          .send({ message: "You can't remove a like before giving one!" });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "There was an issue in the controller" });
+    }
+  },
 };
 
 module.exports = CommentController;
