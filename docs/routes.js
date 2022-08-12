353a896dbfd9e06685a13607d8bc68c715162f8e
Jorge Campo <jcampo1982@outlook.com>
2022-08-12 14:10:02 +0200

    component getbyid

--- docs/routes.js
+++ docs/routes.js
@@ -1,6 +1,6 @@
 module.exports = {
   paths: {
-    '/routes': {
+    '/routes/getAlldb': {
       get: {
         tags: {
           Routes : ' Get Routes',
@@ -14,11 +14,47 @@ module.exports = {
             content: {
               'application/json': {
                 schema: {
-                  $ref: '#/components/schemas/route',
+                  $ref: '#/components/schemas/getRoute',
                 },
               },
             },
           },
+        },
+      },
+    },
+    '/routes/getByIddb/{_id}': {
+      get: {
+        security: [
+          {
+            ApiKeyAuth:[],
+          },
+        ],
+        tags: {
+          Routes : ' Get Routes by Id',
+        },
+        description: 'Get routes by Id',
+        operationId: 'getRoutesById',
+        parameters: [
+          {
+            in: "path",
+            name: "_id",
+            schema: {
+              type: "objectId",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: 'Route were obtained',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/getRouteById',
+                },
+              },
+            },
+          },
+          500: { description: "Server error" },
         },
       },
     },
